acb142891ec86b704f29239e5bb0781107c9e74c
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-06-02 17:26:12 +0300

    feat: added PUT route to properties

--- server/property-viewer/src/main/java/com/skenariolabs/propertyviewer/controllers/PropertyController.java
+++ server/property-viewer/src/main/java/com/skenariolabs/propertyviewer/controllers/PropertyController.java
@@ -2,6 +2,7 @@ package com.skenariolabs.propertyviewer.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import com.skenariolabs.propertyviewer.model.Property;
 import com.skenariolabs.propertyviewer.repository.PropertyRepository;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -62,5 +64,44 @@ public class PropertyController {
     } catch (Exception e) {
       return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+  }
+
+  @PutMapping("/properties/{userId}/{propertyId}")
+  public ResponseEntity<Property> updateProperty(
+    @PathVariable("userId") long userId,
+    @PathVariable("propertyId") long propertyId,
+    @RequestBody Property property
+  ) {
+    try {
+      Optional<Property> propertyData = propertyRepository.findById(propertyId);
+      if (propertyData.isPresent()) {
+        Property _property = propertyData.get();
+        _property.setUserId(userId);
+        _property.setName(property.getName());
+        _property.setStreet(property.getStreet());
+        _property.setPostalCode(property.getPostalCode());
+        _property.setCity(property.getCity());
+        _property.setMunicipality(property.getMunicipality());
+        _property.setCountry(property.getCountry());
+        _property.setDescription(property.getDescription());
+
+        return new ResponseEntity<>(
+          propertyRepository.save(_property),
+          HttpStatus.OK
+        );
+      } else {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+      }
+    } catch (Exception e) {
+      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  public PropertyRepository getPropertyRepository() {
+    return propertyRepository;
+  }
+
+  public void setPropertyRepository(PropertyRepository propertyRepository) {
+    this.propertyRepository = propertyRepository;
   }
 }
