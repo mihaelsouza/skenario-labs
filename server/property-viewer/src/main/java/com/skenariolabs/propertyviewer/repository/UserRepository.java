9e8263397fb40565ac02067aa0d6422ab2daf50e
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-06-02 19:47:14 +0300

    refactor: findByEmail returns an Optional to expose .isPresent method

--- server/property-viewer/src/main/java/com/skenariolabs/propertyviewer/repository/UserRepository.java
+++ server/property-viewer/src/main/java/com/skenariolabs/propertyviewer/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.skenariolabs.propertyviewer.repository;
 
+import java.util.Optional;
+
 import com.skenariolabs.propertyviewer.model.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-  User getByEmail(String email);
+  Optional<User> findByEmail(String email);
 
 }
