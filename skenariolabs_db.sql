7eb87fae9f19ad3ac778e4009b3e101defcb956d
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-06-02 14:52:14 +0300

    feat: database initializer script for Postgres

---
+++ skenariolabs_db.sql
@@ -0,0 +1,38 @@
+drop database propertyviewer;
+drop owned by skenariolabs;
+drop user skenariolabs;
+
+create user skenariolabs with password 'unsecurepassword';
+create database propertyviewer with template=template0 owner=skenariolabs;
+
+alter default privileges grant all on tables to skenariolabs;
+alter default privileges grant all on sequences to skenariolabs;
+
+\connect propertyviewer;
+
+create table users(
+  user_id integer primary key not null,
+  username varchar(20) not null,
+  email varchar(30) not null,
+  password text not null
+);
+
+create table properties(
+  property_id integer primary key not null,
+  user_id integer not null,
+  name varchar(30) not null,
+  street varchar(50) not null,
+  postal_code varchar(30) not null,
+  city varchar(50) not null,
+  municipality varchar(50) not null,
+  country varchar(50) not null,
+  description varchar(1000) not null
+);
+alter table properties add constraint prop_users_fk
+foreign key (user_id) references users(user_id);
+
+alter table users owner to skenariolabs;
+alter table properties owner to skenariolabs;
+
+create sequence sl_users_seq increment 1 start 1;
+create sequence sl_properties_seq increment 1 start 1;
