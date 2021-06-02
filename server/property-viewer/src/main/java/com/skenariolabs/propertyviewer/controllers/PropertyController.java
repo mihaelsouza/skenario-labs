d0040de470064d3f590b7a92677c78fdc7b60851
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-06-02 17:37:18 +0300

    feat: added delete method to PropertyController

--- server/property-viewer/src/main/java/com/skenariolabs/propertyviewer/controllers/PropertyController.java
+++ server/property-viewer/src/main/java/com/skenariolabs/propertyviewer/controllers/PropertyController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -92,6 +93,19 @@ public class PropertyController {
       } else {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
       }
+    } catch (Exception e) {
+      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @DeleteMapping("/properties/{userId}/{propertyId}")
+  public ResponseEntity<HttpStatus> updateProperty(
+    @PathVariable("userId") long userId,
+    @PathVariable("propertyId") long propertyId
+  ) {
+    try {
+      propertyRepository.deleteById(propertyId);
+      return new ResponseEntity<>(HttpStatus.OK);
     } catch (Exception e) {
       return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
