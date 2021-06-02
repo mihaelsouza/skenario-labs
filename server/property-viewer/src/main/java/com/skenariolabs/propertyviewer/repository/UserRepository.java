13944a7749796af3d9ba082fd439d322172b7537
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-06-02 17:55:47 +0300

    fix: countByEmail now returns a user and is named getByEmail

--- server/property-viewer/src/main/java/com/skenariolabs/propertyviewer/repository/UserRepository.java
+++ server/property-viewer/src/main/java/com/skenariolabs/propertyviewer/repository/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-  Integer countByEmail(String email);
+  User getByEmail(String email);
 
 }
