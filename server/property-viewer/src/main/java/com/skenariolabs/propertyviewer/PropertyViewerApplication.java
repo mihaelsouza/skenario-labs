bd2861453930d564042ad75aa93f1fa78f9befdf
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-06-02 14:56:39 +0300

    feat: boilerplate for SpringBoot server using Postgres and JPA

---
+++ server/property-viewer/src/main/java/com/skenariolabs/propertyviewer/PropertyViewerApplication.java
@@ -0,0 +1,13 @@
+package com.skenariolabs.propertyviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyViewerApplication.class, args);
+	}
+
+}
---
+++ server/property-viewer/src/test/java/com/skenariolabs/propertyviewer/PropertyViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skenariolabs.propertyviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
