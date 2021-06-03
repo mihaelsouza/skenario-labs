3d82ff1dea11d32e0c314241f1b092a6974e54f8
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-06-03 18:37:53 +0300

    feat: connected property components to Dashboard

--- client/src/components/Dashboard/Dashboard.tsx
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -1,17 +1,17 @@
 import './Dashboard.css';
 import React, { useEffect } from 'react';
 
-import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { useAppDispatch } from '../../redux/hooks';
 import { updateUser } from '../../redux/usersSlice';
 import { openModal } from '../../redux/modalSlice';
 
 import Modal from '../../containers/Modal/Modal';
 import UserLoginRegister from '../UserLoginRegister/UserLoginRegister';
 import NavBar from '../../containers/NavBar/NavBar';
+import PropertyGrid from '../../containers/PropertyGrid/PropertyGrid';
 
 const Dashboard: React.FC = () => {
   const dispatch = useAppDispatch();
-  const user = useAppSelector(state => state.users);
 
   useEffect(() => {
     const userData = localStorage.getItem('sklabs-propertyviewer-userdata');
@@ -20,8 +20,9 @@ const Dashboard: React.FC = () => {
   }, [dispatch]);
 
   return (
-    <div>
+    <div className="dashboard-container">
       <NavBar />
+      <PropertyGrid />
       <Modal>
         <UserLoginRegister />
       </Modal>
