f11a2c357e600c3ad892c52faa069ef21735150a
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-06-03 15:12:12 +0300

    fix: added variable error boundaries based on http status to getUser

--- client/src/services/ServerService.ts
+++ client/src/services/ServerService.ts
@@ -15,7 +15,9 @@ async function getUser(email: String, password: String): Promise<User> {
 
     return await response.data;
   } catch (e) {
-    throw new Error(e);
+    if (e.response.status === 403) throw new Error('Invalid e-mail and/or password.');
+    if (e.response.status === 404) throw new Error('E-mail not registered.');
+    else throw new Error('Sorry, something went wrong. Try again!')
   }
 };
 
